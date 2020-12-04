caac9a3d17beeb9226a618063c825d2d80f88d42
NewDDay <ZyryanovDmM@yandex.ru>
2020-12-04 21:33:44 +0300

    added readme

--- Plots/error_bars/Errorbars.jl
+++ Plots/error_bars/Errorbars.jl
@@ -7,21 +7,26 @@ n = 10
 y = [mean(sd * randn(n)) for sd in sds]
 errs = 1.96 * sds/sqrt(n)
 
-plot(y, ylims = (-1, 1))
-
-plot(y, ylims = (-1, 1), err = errs)
-# savefig("errorBar1.pdf")
+plot(y, ylims = (-1, 1),
+    err = errs,
+    label = false,
+    title = "График с симметричными ошибками")
+savefig("error_bar1.png")
 
 plot(y,
     1:length(y),
     xerr = errs,
-    marker = stroke(3, :orange))
-# savefig("errorBar2.pdf")
+    marker = stroke(2, :orange),
+    label = false,
+    title = "Повёрнутый график")
+savefig("error_bar_2.png")
 
 plot(y,
     ribbon = errs,
-    fill = :cyan)
-# savefig("errorBar3.pdf")
+    fill = :cyan,
+    label = false,
+    title = "График с залитой зоной ошибок")
+savefig("error_bar_3.png")
 
 n = 10
 x = [(rand()+1) .* randn(n) .+ 2i for i in 1:5]
@@ -44,5 +49,6 @@ plot(x, y,
 plot(x, y,
     xerr = (0.5xerr, 2xerr),
     yerr = (0.5yerr, 2yerr),
-    marker = stroke(3, :orange))
-# savefig("errorBar5.pdf")
+    marker = stroke(2, :orange),
+    label = false)
+savefig("error_bar_4.png")
