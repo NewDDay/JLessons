2374f841a7504375b29734dbfa01e51800881ef1
NewDDay <ZyryanovDmM@yandex.ru>
2020-12-04 16:14:05 +0300

    added parametric func

---
+++ Plots/parametric_functions/parfunc.jl
@@ -0,0 +1,15 @@
+using Plots
+pyplot()
+
+x(t) = sin(t)
+y(t) = sin(2t)
+plot(x, y, 0 , 2π,
+    legend = false,
+    linewidth = 3)
+
+
+x(t) = sin(t)
+y(t) = sin(2t)
+plot(x, y, 0 , 2π,
+    legend = false,
+    linewidth = 3)
